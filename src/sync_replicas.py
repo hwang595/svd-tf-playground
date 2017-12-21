4e4102f762bf2faa7e3606501b4e15c41b0ab281
hwang595 <hongyiwang@cs.wisc.edu>
2017-12-20 22:00:17 -0600

    debug 11

--- src/sync_replicas.py
+++ src/sync_replicas.py
@@ -196,35 +196,36 @@ class LowCommSync(tf.train.SyncReplicasOptimizer):
             # after the decoding is done:, we aggregate them into aggregator:
             #for grad, var in grads_and_vars:
             with ops.device(global_step.device), ops.name_scope(""):
-                for grad, var in decoded_grads_and_vars:
-                    var_list.append(var)
-                    with ops.device(var.device):
-                        # Dense gradients.
-                        #########################################Test Grad Type Here###########################################
-                        if grad is None:
-                            aggregated_grad.append(None)  # pass-through.
-                            continue
-                        elif isinstance(grad, ops.Tensor):
-                            grad_accum = data_flow_ops.ConditionalAccumulator(
-                                grad.dtype,
-                                shape=var.get_shape(),
-                                shared_name=var.name + "/grad_accum")
-                            train_ops.append(grad_accum.apply_grad(
-                                grad, local_step=self._local_step))
-                            aggregated_grad.append(grad_accum.take_grad(
-                                self._replicas_to_aggregate))
-                        else:
-                            with ops.control_dependencies([logging_ops.Print(0, [0], message="Grad is what we don't know!")]):
-                                if not isinstance(grad, ops.IndexedSlices):
-                                    raise ValueError("Unknown grad type!")
-                                grad_accum = data_flow_ops.SparseConditionalAccumulator(
-                                    grad.dtype, shape=(), shared_name=var.name + "/grad_accum")
-                                train_ops.append(grad_accum.apply_indexed_slices_grad(
+                with ops.control_dependencies([logging_ops.Print(0, [0], message="Start Aggregating on Master!")]):
+                    for grad, var in decoded_grads_and_vars:
+                        var_list.append(var)
+                        with ops.device(var.device):
+                            # Dense gradients.
+                            #########################################Test Grad Type Here###########################################
+                            if grad is None:
+                                aggregated_grad.append(None)  # pass-through.
+                                continue
+                            elif isinstance(grad, ops.Tensor):
+                                grad_accum = data_flow_ops.ConditionalAccumulator(
+                                    grad.dtype,
+                                    shape=var.get_shape(),
+                                    shared_name=var.name + "/grad_accum")
+                                train_ops.append(grad_accum.apply_grad(
                                     grad, local_step=self._local_step))
-                                aggregated_grad.append(grad_accum.take_indexed_slices_grad(
+                                aggregated_grad.append(grad_accum.take_grad(
                                     self._replicas_to_aggregate))
-                        #####################################################################################################
-                        self._accumulator_list.append((grad_accum, var.device))
+                            else:
+                                with ops.control_dependencies([logging_ops.Print(0, [0], message="Grad is what we don't know!")]):
+                                    if not isinstance(grad, ops.IndexedSlices):
+                                        raise ValueError("Unknown grad type!")
+                                    grad_accum = data_flow_ops.SparseConditionalAccumulator(
+                                        grad.dtype, shape=(), shared_name=var.name + "/grad_accum")
+                                    train_ops.append(grad_accum.apply_indexed_slices_grad(
+                                        grad, local_step=self._local_step))
+                                    aggregated_grad.append(grad_accum.take_indexed_slices_grad(
+                                        self._replicas_to_aggregate))
+                            #####################################################################################################
+                            self._accumulator_list.append((grad_accum, var.device))
 
                 aggregated_grads_and_vars = zip(aggregated_grad, var_list)
 
